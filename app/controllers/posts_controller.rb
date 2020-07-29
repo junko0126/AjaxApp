aa818479add6ab472e00cb8924d44b6ee0df16e1
junko0126 <hikari2011h.j@gmail.com>
2020-07-29 18:04:01 +0900

    既読機能のフロント実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")
   end
-  
+
   def create
     Post.create(content: params[:content])
-      redirect_to action: :index 
+    redirect_to action: :index
   end
 
   def checked
